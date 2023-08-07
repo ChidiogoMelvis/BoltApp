d2a89b5a3c93820b3c69f29f8335b1fd1c77416c
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-08-07 10:37:41 +0100

    updated

---
+++ BoltApp/BoltApp/Controllers/Cell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  BoltApp
+//
+//  Created by Mac on 07/08/2023.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
