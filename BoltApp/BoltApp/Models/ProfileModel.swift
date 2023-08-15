49ec5a8d2983b6d3a7604b747193fc96174a212e
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-08-15 23:48:36 +0100

    updated

---
+++ BoltApp/BoltApp/Models/ProfileModel.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileModel.swift
+//  BoltApp
+//
+//  Created by Mac on 15/08/2023.
+//
+
+import Foundation
+
+struct ProfileModel {
+    var profileImage: String
+    var profileName: String
+    var viewProfile: String
+    var journey: String
+    var payments: Int
+    var history: String
+    var promotions: String
+    var support: String
+    var about: String
+    var workRides: String
+}
